2ef552255b00c6b794fdd16a3a1c11fa24051344
jhuniorQUIGA <48796947+jhuniorQUIGA@users.noreply.github.com>
2019-03-25 12:19:13 -0400

    Add files via upload

---
+++ Aritmetica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmetica;
+
+/**
+ *
+ * @author jhunior
+ */
+public class Aritmetica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public int Suma(int numero1, int numero2)
+    {
+        // initialise instance variables
+        int res = numero1 + numero2;
+        return res;
+    
+    }
+    
+}
